acfffb834c03c5f8885332847a7a31e229ae4a9c
Daniel Daza <dazuku.rulo@gmail.com>
2014-11-04 11:43:47 -0500

    Add description to item of search

--- src/common/searchView/searchView.js
+++ src/common/searchView/searchView.js
@@ -1,20 +1,53 @@
 (function(app) {
-	var SearchViewController = function($scope, ProductService) {
+	var SearchViewController = function($scope, ProductService, hotkeys) {
 		var self = this;
 
 		$scope.getTitle = function() {
-			return $scope.keyword + ' - 0 results found';
+			return $scope.keyword + ' - ' + self.results.length + ' results found';
 		};
 
 		self.results = [];
 		self.resultButtons = [];
 
+		self.isItemSelected = false;
+		self.itemSelected = 0;
+
 		var init = function() {
+			hotkeys.add({
+				combo: 'up',
+				callback: function(event) {
+					if(self.itemSelected - 1 >= 0) {
+						self.resultButtons[self.itemSelected--].active = false;
+						self.resultButtons[self.itemSelected].active = true;
+
+						$scope.selected = self.results[self.itemSelected];
+					}
+					
+					event.preventDefault();
+				}
+			});
+
+			hotkeys.add({
+				combo: 'down',
+				callback: function(event) {
+					if(self.itemSelected + 1 < self.resultButtons.length) {
+						self.resultButtons[self.itemSelected++].active = false;
+						self.resultButtons[self.itemSelected].active = true;
+
+						$scope.selected = self.results[self.itemSelected];
+					}
+
+					event.preventDefault();
+				}
+			});
+
 			$scope.$watch('keyword', function(newValue, oldValue) {
 				var searchPremise = ProductService.getListFromSearchWithKey(newValue);
 
 				searchPremise.then(function(res) {
 					self.results = res.data.products;
+
+					console.log(self.results);
 
 					self.resultButtons = [];
 
@@ -24,6 +57,15 @@
 							label: label,
 							active: false,
 						});
+					}
+
+					if(self.results.length !== 0) {
+						self.itemSelected = 0;
+						self.resultButtons[self.itemSelected].active = true;
+						$scope.selected = self.results[0];
+						self.isItemSelected = true;
+					} else {
+						self.isItemSelected = false;
 					}
 					
 				});
@@ -45,7 +87,7 @@
 		};
 	};
 
-	app.controller('SearchViewController', ['$scope', 'ProductService', SearchViewController]);
+	app.controller('SearchViewController', ['$scope', 'ProductService', 'hotkeys', SearchViewController]);
 	app.directive('searchView', SearchViewDirective);
 
 }(angular.module("caracolplaylgtvapp.searchView", [
