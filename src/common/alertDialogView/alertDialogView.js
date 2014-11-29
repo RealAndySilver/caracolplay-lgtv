4cbc9d96d10b293ab70e043b9b9fe7e581f51488
Daniel Daza <dazuku.rulo@gmail.com>
2014-11-28 22:50:43 -0500

    Show alertDialog like a modal dialog

--- src/common/alertDialogView/alertDialogView.js
+++ src/common/alertDialogView/alertDialogView.js
@@ -1,26 +1,37 @@
 (function(app) {
+	/** Start code for testing **/
+	app.config(['$stateProvider', function ($stateProvider) {
+		$stateProvider.state('alertDialogView', {
+			url: '/alertdialogview',
+			views: {
+				"main": {
+					controller: 'AlertDialogViewController',
+					//templateUrl: 'alertDialogView/alertDialogView.tpl.html'
+				}
+			},
+			data:{ pageTitle: 'AlertDialogView' }
+		});
+	}]);
+	/** End code for testing **/
 
-    app.config(['$stateProvider', function ($stateProvider) {
-        $stateProvider.state('alertDialogView', {
-            url: '/alertdialogview',
-            views: {
-                "main": {
-                    controller: 'AlertDialogViewController',
-                    templateUrl: 'alertDialogView/alertDialogView.tpl.html'
-                }
-            },
-            data:{ pageTitle: 'AlertDialogView' }
-        });
-    }]);
+	var AlertDialogViewController = function($scope, $modal) {
+		var init = function() {
+			var modalInstance = $modal.open({
+				templateUrl: 'alertDialogView/alertDialogView.tpl.html',
+				size: '',
+				resolve: {
+					items: function () {
+						return $scope.items;
+					}
+				}
+			});
+		};
 
-    app.controller('AlertDialogViewController', ['$scope', function ($scope) {
+		init();
+	};
 
-        var init = function() {
-        };
-
-        init();
-    }]);
+	app.controller('AlertDialogViewController', ['$scope', '$modal', AlertDialogViewController]);
 
 }(angular.module("caracolplaylgtvapp.alertDialogView", [
-    'ui.router'
+	'ui.router'
 ])));
