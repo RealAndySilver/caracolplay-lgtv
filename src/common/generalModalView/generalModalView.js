12fbb61c91433faa8174663bda9692e488f8acc1
Daniel Daza <dazuku.rulo@gmail.com>
2015-04-01 09:11:26 -0500

    I make the visual things of the Genral Modal VIew

--- src/common/generalModalView/generalModalView.js
+++ src/common/generalModalView/generalModalView.js
@@ -5,8 +5,7 @@
 			url: '/generalmodalview',
 			views: {
 				"main": {
-					controller: 'GeneralModalViewController as model',
-					templateUrl: 'generalModalView/generalModalView.tpl.html'
+					controller: 'GeneralModalViewRouteController as self',
 				}
 			},
 			data: {
@@ -15,15 +14,70 @@
 		});
 	});
 
-	module.controller('GeneralModalViewController', [function() {
-		var model = this;
+	var GeneralModalViewService = function($modal) {
+		var self = this;
+		self.modalInstance = {};
+
+		init();
+
+		self.show = function(image, text, onDismiss) {
+			self.modalInstance = $modal.open({
+				controller: 'GeneralModalViewController',
+				templateUrl: 'generalModalView/generalModalView.tpl.html',
+				size: 'lg',
+				resolve: {
+					modalInfo: function() {
+						return {
+							'image': image,
+							'text': text,
+						};
+					}
+				}
+			});
+
+			self.modalInstance.result.then(function() {
+				if(onDismiss) {
+					onDismiss();
+				}
+			}, function() {
+				if(onDismiss) {
+					onDismiss();
+				}
+			});
+		};
+
+		self.dismiss = function() {
+			self.dismiss('cancel');
+		};
+
+		function init() {
+
+		}
+	};
+
+	var GeneralModalViewController = function() {
+		var self = this;
 
 		init();
 
 		function init() {
 
 		}
-	}]);
+	};
+
+	var GeneralModalViewRouteController = function(GeneralModalViewService) {
+		var self = this;
+
+		init();
+
+		function init() {
+			GeneralModalViewService.show('title', 'text');
+		}
+	};
+
+	module.service('GeneralModalViewService', ['$modal', GeneralModalViewService]);
+	module.controller('GeneralModalViewController', [GeneralModalViewController]);
+	module.controller('GeneralModalViewRouteController', ['GeneralModalViewService', GeneralModalViewRouteController]);
 
 }(angular.module("caracolplaylgtvapp.generalModalView", [
 	'ui.router'
